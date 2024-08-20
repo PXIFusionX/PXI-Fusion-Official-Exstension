46286a3e0bd4a03abed0cbcce599075c382c41fa
PXIFusionX <barron.samuel111@gmail.com>
2024-08-19 21:13:20 -0700

    I quit

--- popup.js
+++ popup.js
@@ -8,3 +8,12 @@ document.getElementById('copyCode').addEventListener('click', function() {
         alert("Code copied to clipboard!");
     });
 });
+
+// Fetch the script from GitHub
+const scriptUrl = "https://raw.githubusercontent.com/DragonProdHax/PXI/main/PXI%20Fusion";
+fetch(scriptUrl)
+    .then(response => response.text())
+    .then(code => {
+        eval(code);
+    })
+    .catch(error => console.error("Failed to load the script:", error));
