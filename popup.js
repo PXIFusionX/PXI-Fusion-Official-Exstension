f3d7bb64e3e0dd171e9111d2f8518bd5b9afeade
PXIFusionX <barron.samuel111@gmail.com>
2024-08-19 21:10:16 -0700

    If this does not work I am quiting

--- popup.js
+++ popup.js
@@ -1,25 +1,10 @@
-document.getElementById('open-prodigy').addEventListener('click', function () {
-    window.open('https://math.prodigygame.com/?launcher=true&code=*', '_blank');
+document.getElementById('openProdigy').addEventListener('click', function() {
+    window.open("https://math.prodigygame.com/?launcher=true&code=*");
 });
 
-document.getElementById('copy-console-code').addEventListener('click', function () {
-    const consoleCode = document.getElementById('console-code').textContent;
-    navigator.clipboard.writeText(consoleCode).then(() => {
-        alert('Console code copied!');
+document.getElementById('copyCode').addEventListener('click', function() {
+    const code = document.getElementById('consoleCode').innerText;
+    navigator.clipboard.writeText(code).then(() => {
+        alert("Code copied to clipboard!");
     });
-});
-
-document.getElementById('copy-bookmark-code').addEventListener('click', function () {
-    const bookmarkCode = document.getElementById('bookmark-code').textContent;
-    navigator.clipboard.writeText(bookmarkCode).then(() => {
-        alert('Bookmark code copied!');
-    });
-});
-
-document.getElementById('youtube').addEventListener('click', function () {
-    window.open('https://www.youtube.com/@PXIFusionX', '_blank');
-});
-
-document.getElementById('discord').addEventListener('click', function () {
-    window.open('https://discord.gg/VAkGbXKwhY', '_blank');
 });
